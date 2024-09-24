8593c80089e4163c30c885631dfaf5b9bf988f60
karan Mohite <mohitekaran18@gmail.com>
2024-09-24 14:41:44 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const router = require("./routes/router");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+mongoose.set('strictQuery', true); // Add this line
+
+const staticPath = path.join(__dirname, "public");
+app.use(express.static(staticPath));
+
+mongoose.connect("mongodb+srv://om:pharate11@cluster0.nnceu.mongodb.net/your-database-name", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.error(err));
+
+app.set("view engine", "ejs");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', router);
+
+app.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
+});
---
+++ routes/router.js
@@ -0,0 +1,11 @@
+const express =  require("express");
+const router = express.Router();
+const stuController = require("../controller/stuController");
+const teaController = require("../controller/teaController");
+const lecController = require("../controller/lecController");
+const adminController = require("../controller/adminController");
+const upperController = require("../controller/upperController");
+const middleWare = require("../middleware/auth")
+router.get('/',upperController.home_page);
+router.get('/home',upperController.home_page);
+module.exports = router;
